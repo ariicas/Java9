3ae8b378aca928f33874a41a8ba31c7b8caef5ad
ariicas <ariicastrobustoss@gmail.com>
2021-03-11 20:56:21 -0500

    Lalaland

--- src/testgit/Git.java
+++ src/testgit/Git.java
@@ -4,13 +4,10 @@ public class Git {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int num=1;
-		int num2=27;
-		int sum=0;
+	
 		
-		sum=num+num2;
-		
-		System.out.println("My name is Ariana and I am "+sum);
+		System.out.println("this is  GIT ");
+		System.out.println(" we git ");
 	}
 
 }
